3ad8de1869c503cee6b6040432fd8e5c90750ebd
Shi Chunchen <shichunchen@co188.com>
2016-10-25 17:19:19 +0800

    Initial Commit

---
+++ SCCBBS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCCBBS
+//
+//  Created by co188 on 16/10/25.
+//  Copyright © 2016年 co188. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
